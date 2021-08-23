0f6a960499ede6a6bc17b46d51f4a22d7e49993e
iudini <hoiitzx@gmail.com>
2021-08-23 13:57:57 +0300

    0. Spring test [#6881]

--- src/test/java/ru/job4j/forum/control/PostControlTest.java
+++ src/test/java/ru/job4j/forum/control/PostControlTest.java
@@ -6,12 +6,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.service.CommentService;
+import ru.job4j.forum.service.PostService;
+
+import java.util.ArrayList;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
@@ -20,9 +27,16 @@ class PostControlTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private PostService postService;
+    @MockBean
+    private CommentService commentService;
+
     @Test
     @WithMockUser
     void post() throws Exception {
+        Mockito.when(postService.getById(Mockito.anyLong())).thenReturn(new Post());
+        Mockito.when(commentService.getById(Mockito.anyLong())).thenReturn(new ArrayList<>());
         this.mockMvc.perform(get("/post").param("id", "1"))
                 .andDo(print())
                 .andExpect(status().isOk())
